36437fc220727ab62778e261b39675026d8f0d3c
vova91 <v.korniienko91@gmail.com>
2019-06-12 11:41:27 +0300

    changes in windows function

--- detectStorage_windows.go
+++ detectStorage_windows.go
@@ -1,33 +1,28 @@
 package detect_storage
 
-// #include <stdlib.h>
-import "C"
 import (
-	"syscall"
 	"unsafe"
 	"golang.org/x/sys/windows"
 )
 
+const DRIVE_REMOVABLE = 2
+
 func DetectRemovableStorage() ([]string, error) {
-	const DRIVE_REMOVABLE = 2
-	kernel32, _ := syscall.LoadLibrary("kernel32.dll")
-	getLogicalDrivesHandle, _ := syscall.GetProcAddress(kernel32, "GetLogicalDrives")
 	kernelDll := windows.NewLazyDLL("kernel32.dll")
 	pGetDriveTypeA := kernelDll.NewProc("GetDriveTypeA")
+	pGetLogicalDrives := kernelDll.NewProc("GetLogicalDrives")
 	var drives []string
 
-	bitMap, _, err := syscall.Syscall(uintptr(getLogicalDrivesHandle), 0, 0, 0, 0)
-	if err != 0 {
+	bitMap, _, err := pGetLogicalDrives.Call()
+	if err == nil {
 		return nil, err
 	} else {
 		for i := 'A'; i <= 'Z'; i++ {
 			if bitMap&1 == 1 {
 				droot := string(i) + ":\\"
-				drootC := C.CString(droot)
-				tp, _, _ := pGetDriveTypeA.Call(uintptr(unsafe.Pointer(drootC)))
-				defer C.free(unsafe.Pointer(drootC))
+				tp, _, _ := pGetDriveTypeA.Call(uintptr(unsafe.Pointer(&[]byte(droot)[0])))
 				if tp == DRIVE_REMOVABLE {
-					drives = append(drives, string(i)+":\\")
+					drives = append(drives, string(i) + ":\\")
 				}
 			}
 			bitMap >>= 1
